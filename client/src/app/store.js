bf25f76a8bee4a627ebad4f9854815458dbe055b
Kojo <boakyeni@usc.edu>
2023-07-07 17:04:11 -0700

    added 404 page

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-
+import propertyReducer from '../features/properties/propertySlice';
 
 export const store = configureStore({
   reducer: {
-    
+    properties: propertyReducer,
   },
+
 });
+window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
